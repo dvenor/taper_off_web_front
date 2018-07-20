b1996cc1ebb82a7bf17b60837d88eefccc30126a
dvenor <danielvenor@Daniels-MacBook-Pro.local>
2018-07-19 17:02:25 -0700

    Changed the title to coming soon.

--- src/App.js
+++ src/App.js
@@ -11,8 +11,14 @@ class App extends Component {
           <h1 className="App-title">Coming soon.</h1>
         </header>
         <p className="App-intro">
-          The taper app to help you find your proper dosing schedule.
-          To get started, edit <code>src/App.js</code> and save to reload.
+          Over 10 million americans regularly take anti-depresant medication.  It is an effective treatment option
+          and has helped many people lead productive, happy lives. 
+          <br/><br/>
+          However, physicians don't normally spend the time to explain the process to ween off these medications.
+          <br/><br/>
+          This application is a tool to assist in finding the proper taper schedule.
+          <br/>
+          ALWAYS CONSULT YOUR DOCTOR BEFORE MAKING ANY CHANGES TO YOUR MEDICATION!   
         </p>
       </div>
     );
