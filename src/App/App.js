b1a3706ec11bbad4688090ae7f14324b5f5545fb
dvenor <danielvenor@Daniels-MacBook-Pro.local>
2018-08-11 15:01:48 -0700

    New wording on the main message

--- src/App/App.js
+++ src/App/App.js
@@ -99,7 +99,7 @@ class App extends Component {
         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossOrigin="anonymous"></link>
         <header className="App-header">
           {/* <img src={logo} className="App-logo" alt="logo" /> */}
-          <h1 className="App-title">Taper Off - Assisting with SSRI Discontinuation</h1>
+          <h1 className="App-title">Taper Off - Microtaper Calculator</h1>
         </header>
         <Grid className="container-fluid">
           <Row>
